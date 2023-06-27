bcadc711a36f917db662950ce75f4ae643b182cc
tsauvage <ssauvage.timothee@gmail.com>
2023-06-27 16:02:51 +0200

    setup models

--- paperless/models.py
+++ paperless/models.py
@@ -6,8 +6,16 @@ from django.db import models
 class Facture(models.Model):
     location = models.CharField(max_length=32)
 
+    class Meta:
+        db_table="facture"
+        app_label='paperless'
+
 class User(models.Model):
     siret = models.BigIntegerField(primary_key=True)
     nom = models.CharField(max_length=128)
     password = models.CharField(max_length=258)
     factures = models.ManyToManyField(Facture)
+
+    class Meta():
+        db_table='user'
+        app_label='paperless'
