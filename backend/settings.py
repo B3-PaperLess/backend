3904b2024d4fece1f92426460b2cf0e8b6ee76a1
Cédric Brunet <brunetced42@gmail.com>
2023-06-27 14:25:10 +0200

    ajout des premières routes et modèles

--- backend/settings.py
+++ backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'paperless'
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = ['localhost','0.0.0.0','127.0.0.1']
